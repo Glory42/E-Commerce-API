85350b9700a81a7ccb193f80af1f0bdcd905a7fc
Glory42 <gorkemkaryol03@gmail.com>
2025-10-24 16:13:00 +0300

    feat: add routes to server.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,16 +1,20 @@
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
-
-dotenv.config();
+import config from './config/config.js';
+import userRoute from './routes/userRoute.js';
+import productRoute from './routes/productRoute.js';
+import orderRoute from './routes/orderRoute.js';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use('/api/user', userRoute);
+app.use('/api/product', productRoute);
+app.use('/api/order', orderRoute);
+
 app.get('/', (req, res) => {
     res.json({ message: 'Server is running :)' });
 })
 
-const port = process.env.PORT;
-app.listen(port, () => console.log(`server is running on http://localhost:${port}`));
+app.listen(config.port, () => console.log(`server is running on http://localhost:${config.port}`));
