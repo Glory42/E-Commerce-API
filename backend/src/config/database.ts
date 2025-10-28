1c3056244472b29ee081ffb04c560b14e49a66aa
Glory42 <gorkemkaryol03@gmail.com>
2025-10-28 17:59:23 +0300

    feat: add supabase connection.

---
+++ backend/src/config/database.ts
@@ -0,0 +1,9 @@
+const { createClient } = require('@supabase/supabase-js');
+import config from './config.js';
+
+const supabaseUrl = config.supabase_url;
+const supabaseKey = config.supabase_key;
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
