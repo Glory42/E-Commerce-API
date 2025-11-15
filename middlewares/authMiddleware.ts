9df7d1911c2565e98a853d3f7a72d677f26222a2
Glory42 <gorkemkaryol03@gmail.com>
2025-11-15 21:39:12 +0300

    fix: add if token is undefiend.

--- middlewares/authMiddleware.ts
+++ middlewares/authMiddleware.ts
@@ -2,13 +2,16 @@ import jwt from 'jsonwebtoken';
 import { Request, Response, NextFunction} from 'express';
 import config from '../config/config.js';
 
-export function authtoken(req: Request, res: Response, next: NextFunction) {
+export default function authtoken(req: Request, res: Response, next: NextFunction) {
     const header = req.headers.authorization;
     if (!header) {
         return res.status(401).json({ message: 'Authorization header missing' });
     }
 
     const token = header.split(' ')[1];
+    if (!token) {
+        return res.status(401).json({ message: 'Token is missind' });
+    }
 
     try {
         const decoded = jwt.verify(token, config.JWT_TOKEN);
