ff2f5813a2d468a08ba3b7e7e69326ec689d468d
Glory42 <gorkemkaryol03@gmail.com>
2025-11-18 01:50:37 +0300

    feat: add get product function.

--- controllers/productController.ts
+++ controllers/productController.ts
@@ -1,18 +1,48 @@
 import { Request, Response } from 'express';
+import Product from '../models/Products.js';
 
 export async function getProducts(req: Request, res: Response) {
     try {
+        const products = await Product.getProducts();
+        if (!products) {
+            res.status(404).json({ message: 'Products not found' });
+        }
+
+        res.status(200).json({
+            message: 'Products get',
+            products,
+        });
 
     } catch (err) {
-
+        if (err instanceof Error) {
+            console.error('Failed to fetch products: ', err.message, err.stack);
+            res.status(500).json({ error: 'server error!', details: err.message || err.toString() });
+        }
     }
 }
 
 export async function getProductById(req: Request, res: Response) {
     try {
+        const { id } = req.params;
+        if (!id) {
+            return res.status(400).json({ message: 'Product id is required' });
+        }
 
-    } catch (err) {
+        const product = await Product.getProductById(id);
+        if (!product) {
+            return res.status(404).json({ message: 'Product not found' });
+        }
+
+        res.status(200).json({
+            message: 'Product get by id',
+            product,
+        });
         
+    } catch (err) {
+        if (err instanceof Error) {
+            console.error('Failed to fetch product: ', err.message, err.stack);
+            res.status(500).json({ error: 'server error!', details: err.message || err.toString() });
+        }
     }
 }
 
@@ -20,7 +50,10 @@ export async function createProduct(req: Request, res: Response) {
     try {
 
     } catch (err) {
-        
+        if (err instanceof Error) {
+            console.error('Failed to create product: ', err.message, err.stack);
+            res.status(500).json({ error: 'server error!', details: err.message || err.toString() });
+        }
     }
 }
 
@@ -28,7 +61,10 @@ export async function createProductReview(req: Request, res: Response) {
     try {
 
     } catch (err) {
-        
+        if (err instanceof Error) {
+            console.error('Failed to create product review: ', err.message, err.stack);
+            res.status(500).json({ error: 'server error!', details: err.message || err.toString() });
+        }
     }
 }
 
@@ -36,7 +72,10 @@ export async function updateProduct(req: Request, res: Response) {
     try {
 
     } catch (err) {
-        
+        if (err instanceof Error) {
+            console.error('Failed to update product: ', err.message, err.stack);
+            res.status(500).json({ error: 'server error!', details: err.message || err.toString() });
+        }
     }
 }
 
@@ -44,6 +83,9 @@ export async function deleteProduct(req: Request, res: Response) {
     try {
 
     } catch (err) {
-        
+        if (err instanceof Error) {
+            console.error('Failed to delete product: ', err.message, err.stack);
+            res.status(500).json({ error: 'server error!', details: err.message || err.toString() });
+        }
     }
 }
