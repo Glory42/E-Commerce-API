4df643244227447544e3dc686a56bc60be5328d8
Glory42 <gorkemkaryol03@gmail.com>
2025-11-18 23:39:18 +0300

    feat: add more interface to use products.

--- types/Product.ts
+++ types/Product.ts
@@ -1,6 +1,28 @@
-export default interface Products {
+export interface ProductDTO {
     name: string,
     price: number,
     stock: number | boolean,
     category: string,
+}
+
+export interface UpdateProductDTO {
+    name?: string;
+    price?: number;
+    stock?: number;
+    category?: string | boolean;
+}
+
+export interface CreateProductDTO {
+    name: string,
+    price: number,
+    stock: string | boolean,
+    category: string
+}
+
+export interface ReviewDTO {
+    id?: string;
+    userId?: string;
+    rating: number;
+    comment?: string;
+    createdAt?: string;
 }
