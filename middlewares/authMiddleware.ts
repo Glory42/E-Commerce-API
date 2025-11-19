35e3d2b80810b682669fc3b9634c92cdd34808be
Glory42 <gorkemkaryol03@gmail.com>
2025-11-19 22:22:48 +0300

    fix: change jwt verifing for new global user type.

--- middlewares/authMiddleware.ts
+++ middlewares/authMiddleware.ts
@@ -15,7 +15,18 @@ export default function authtoken(req: Request, res: Response, next: NextFunctio
 
     try {
         const decoded = jwt.verify(token, config.JWT_TOKEN);
-        req.user = decoded;
+        if (typeof decoded === 'string') {
+            return res.status(500).json({ error: 'Invalid token payload' });
+        }
+
+        req.user = {
+            id: decoded.id,
+            email: decoded.email,
+            role: decoded.role,
+            username: decoded.username,
+            phone: decoded.phone,    
+        };
+        
         next();
     } catch (err) {
         return res.status(500).json({ error: 'Invalid or expired token' });
