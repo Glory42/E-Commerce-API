c63240b698d2b4ef27851a13bacaac8fa93d10b7
Glory42 <gorkemkaryol03@gmail.com>
2025-11-20 13:10:05 +0300

    feat: add 404 and global error handler.

--- server.ts
+++ server.ts
@@ -4,6 +4,8 @@ import config from './config/config.js';
 import userRoute from './routes/userRoute.js';
 import productRoute from './routes/productRoute.js';
 import orderRoute from './routes/orderRoute.js';
+import notFoundHandler from './middlewares/404handler.js';
+import errorHandler from './middlewares/errorHandler.js';
 
 const app = express();
 app.use(express.json());
@@ -12,6 +14,8 @@ app.use(cors());
 app.use('/api/user', userRoute);
 app.use('/api/product', productRoute);
 app.use('/api/order', orderRoute);
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 app.get('/', (req, res) => {
     res.json({ message: 'Server is running :)' });
