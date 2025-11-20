5ba5c925addf7a69e23c3f7fe0b7a50d72d48145
Glory42 <gorkemkaryol03@gmail.com>
2025-11-20 13:09:16 +0300

    feat: add validate user.

--- models/User.ts
+++ models/User.ts
@@ -7,20 +7,7 @@ import {
     CreateUserDTO, 
     ProfileDTO 
 } from '../types/User.js';
-
-const validateUser = ({ email, password, username, phone}: ValidateUserDTO) => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email)) {
-        throw new Error("Invalid email format");
-    }
-    if (password.length < 8) {
-        throw new Error('Password must be longer than 7 characters');
-    }
-    if (!username || username.length < 3) {
-        throw new Error('User name must be greater than 3 characters');
-    }
-    return true;
-}
+import { validateUser } from '../utils/validator.js';
 
 export default class User {
     static async getUsers(): Promise<UserDTO[]>{
