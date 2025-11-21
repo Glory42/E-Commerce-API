761abedd42734c46bcc9f0fd8ea9731ef9cb7a72
Glory42 <gorkemkaryol03@gmail.com>
2025-11-21 22:51:50 +0300

    refactor: add utils to orderController for repeated try-catch actions.

--- controllers/orderController.ts
+++ controllers/orderController.ts
@@ -1,85 +1,62 @@
 import { Request, Response } from 'express';
 import Order from '../models/Order.js';
+import { asyncHandler } from '../utils/asyncHandler.js';
+import { apiError } from '../utils/apiError.js';
 
-export async function getOrders(req: Request, res: Response) {
-    try {
+export const getOrders = asyncHandler(async (req: Request, res: Response) => {
         const orders = await Order.getOrders();
         if (!orders) {
-            return res.status(404).json({ message: 'Orders not found' });
+            throw new apiError(400, 'Orders not found');
         }
 
         res.status(200).json({
             message: 'Orders fetched successfully',
             orders,
-        });
+        });   
+});
 
-    } catch (err) {
-        if (err instanceof Error) {
-            console.error('Failed to fetch Order:', err.message, err.stack);
-            res.status(500).json({ error: 'server error!', details: err.message || err.toString() });
-        }
-    }
-}
-
-export async function getOrderById(req: Request, res: Response) {
-    try {
+export const getOrderById = asyncHandler(async (req: Request, res: Response) => {
         const { id } = req.params;
         if (!id) {
-            return res.status(400).json({ message: 'Order ID is required' });
+            throw new apiError(400, 'Order ID is required');
         }
 
         const order = await Order.getOrderById(id);
         if (!order) {
-            return res.status(404).json({message: 'Order not found'});
+            throw new apiError(404,'Order not found');
         } 
         
         res.status(200).json({
             message: 'Order fetched successfully',
             order,
         });
+});
 
-    } catch (err) {
-        if (err instanceof Error) {
-            console.error('Failed to fetch Order:', err.message, err.stack);
-            res.status(500).json({ error: 'server error!', details: err.message || err.toString() });
-        }
-    }
-}
-
-export async function getMyOrders(req: Request, res: Response) {
-    try {
+export const getMyOrders = asyncHandler(async (req: Request, res: Response) => {
         const user_id = req.user?.id;
         if (!user_id) {
-            return res.status(400).json({ message: 'User ID is required' });
+            throw new apiError(400, 'User ID is required');
         }
 
         const orders = await Order.getUserOrders(user_id);
         if (!orders) {
-            return res.status(404).json({ message: 'Orders not found' });
+            throw new apiError(404, 'Orders not found');
         }
 
         res.status(200).json({
             message: 'User order fetched successfully',
             orders,
-        })
+        });
+});
 
-    } catch (err) {
-        if (err instanceof Error) {
-            console.error('Failed to fetch Order:', err.message, err.stack);
-            res.status(500).json({ error: 'server error!', details: err.message || err.toString() });
-        }
-    }
-}
-
-export async function addOrderItems(req: Request, res: Response) {
-    try {
+export const addOrderItems = asyncHandler(async (req: Request, res: Response) => {
         const user_id = req.user?.id;
         const { product_id, quantity, total_price } = req.body;
         if (!user_id || !product_id) {
-            return res.status(400).json({ message: 'User and Product IDs are required' });
+            throw new apiError(400, 'User and Product IDs are required');
         }
         if (!quantity || !total_price) {
-            return res.status(400).json({ message: 'Order Informations required' });
+            throw new apiError(400, 'Order Informations required');
         }
 
         const newOrder = await Order.addItems({
@@ -89,93 +66,66 @@ export async function addOrderItems(req: Request, res: Response) {
             total_price,
         });
         if (!newOrder) {
-            return res.status(404).json({ message: 'Order datas not found' });
+            throw new apiError(404, 'Order datas not found');
         }
 
         res.status(201).json({
             message: 'Order added successfully',
             newOrder,
         });
+});
 
-    } catch (err) {
-        if (err instanceof Error) {
-            console.error('Failed to fetch Order:', err.message, err.stack);
-            res.status(500).json({ error: 'server error!', details: err.message || err.toString() });
-        }
-    }
-}
-
-export async function updateOrderPaid(req: Request, res: Response) {
-  try {
+export const updateOrderPaid = asyncHandler(async (req: Request, res: Response) => {
     const orderId = req.params.id;
     if (!orderId) {
-        return res.status(400).json({ message: 'Order ID is required' });
+        throw new apiError(400, 'Order ID is required');
     }
     
     const updatedOrder = await Order.updateOrder(orderId, { paid: true });
     if (!updatedOrder) {
-        return res.status(404).json({ message: 'Order not found' });
+        throw new apiError(404, 'Order not found');
     };
+
     res.status(200).json({
         message: 'Payment successful',
         updatedOrder
     });
+});
 
-  } catch (err) {
-    if (err instanceof Error) {
-      res.status(500).json({ error: 'Server error', details: err.message });
-    }
-  }
-}
-
-export async function updateOrderDelivered(req: Request, res: Response) {
-  try {
+export const updateOrderDelivered = asyncHandler(async (req: Request, res: Response) => {
     const orderId = req.params.id;
     if (!orderId) {
-        return res.status(400).json({ message: 'Order ID is required' });
+        throw new apiError(400, 'Order ID is required');
     }
 
     const updatedOrder = await Order.updateOrder(orderId, { delivered: true });
     if (!updatedOrder) {
-        return res.status(404).json({ message: 'Order not found' });
+        throw new apiError(404, 'Order not found');
     };
 
     res.status(200).json({ 
         message: 'Order marked as delivered',
         updatedOrder,
     });
+});
 
-  } catch (err) {
-    if (err instanceof Error) {
-      res.status(500).json({ error: 'Server error', details: err.message });
-    }
-  }
-}
-
-export async function updateOrderQuantity(req: Request, res: Response) {
-  try {
+export const updateOrderQuantity = asyncHandler(async (req: Request, res: Response) => {
     const orderId = req.params.id;
     if (!orderId) {
-        return res.status(400).json({ message: 'Order ID is required' });
+        throw new apiError(400, 'Order ID is required');
     }
     const { quantity } = req.body;
     if (!quantity) {
-        return res.status(400).json({ message: 'Quantity is required' });
+        throw new apiError(400, 'Quantity is required');
     };
 
     const updatedOrder = await Order.updateOrder(orderId, { quantity });
     if (!updatedOrder) {
-        return res.status(404).json({ message: 'Order not found' });
+        throw new apiError(404, 'Order not found');
     };
 
     res.status(200).json({
         message: 'Order quantity updated',
         updatedOrder,
     });
-
-  } catch (err) {
-    if (err instanceof Error) {
-      res.status(500).json({ error: 'Server error', details: err.message });
-    }
-  }
-}
+});
