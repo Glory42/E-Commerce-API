b11ac6bf2e1aff90c1c177d1b2417ab1010d0d4d
Glory42 <gorkemkaryol03@gmail.com>
2025-11-21 23:18:23 +0300

    refactor: add utils to userController for repeated try-catch actions.

--- controllers/userController.ts
+++ controllers/userController.ts
@@ -1,159 +1,108 @@
 import { Request, Response } from 'express';
 import User from '../models/User.js';
+import { asyncHandler } from '../utils/asyncHandler.js';
+import { apiError } from '../utils/apiError.js';
 
-export async function getUsers(req: Request, res: Response) {
-    try {
-        const users = await User.getUser();
-        if (!users) {
-            res.status(404).json({ message: 'No user found' });
-        }
-
-        res.status(200).json({
-            message: 'Users fetched successfully',
-            users,
-        });
-
-    } catch (err) {
-        if (err instanceof Error) {
-            console.error('Failed to fetch users: ', err.message, err.stack);
-            res.status(500).json({ error: 'server error!', details: err.message || err.toString() });
-        }
+export const getUsers = asyncHandler(async (req: Request, res: Response) => {
+    const users = await User.getUsers();
+    if (!users) {
+        throw new apiError(404, 'No user found');
     }
-}
 
-export async function getUserById(req: Request, res: Response) {
-    try {
+    res.status(200).json({
+        message: 'Users fetched successfully',
+        users,
+    });
+});
+
+export const getUserById = asyncHandler(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    if (!id) {
+        throw new apiError(400, 'User id is required');
+    }
+
+    const user = await User.getUserById(id);
+    if (!user) {
+        throw new apiError(404, 'User not found');
+    }
+    
+    res.status(200).json({
+        message: 'User fetched successfully',
+        user,
+    });
+});
+
+export const getUserProfile = asyncHandler(async (req: Request, res: Response) => {
+    const userId = req.user?.id;
+        if (!userId) {
+        throw new apiError(401, 'Unauthorized - User ID not found');
+    }
+
+    const userProfile = await User.getUserProfile(userId);
+    if (!userProfile) {
+        throw new apiError(404, 'No user found');
+    }
+
+    res.status(200).json({
+        message: 'Public profile fetched successfully',
+        userProfile,
+    });
+});
+
+export const updateUserById = asyncHandler(async (req: Request, res: Response) => {
+    const { id } = req.params;
+    if (!id) {
+        throw new apiError(400, 'User id is required');
+    }
+
+    const updates = req.body;
+    if (!updates) {
+        throw new apiError(400, 'Update data is required');
+    }
+    
+    const updatedUser = await User.updateUser(String(id), updates);
+    if (!updatedUser) {
+        throw new apiError(404, 'User not found or update failed');
+    }
+
+    res.status(200).json({
+        message: 'User updated successfully',
+        updatedUser,
+    });
+});
+
+export const updateUserProfile = asyncHandler(async (req: Request, res: Response) => {
+    const loggedIn = req.user?.id;
+    if (!loggedIn) {
+        throw new apiError(400, 'Profile id is required');
+    }
+
+    const updates = req.body;
+    if (!updates) {
+        throw new apiError(400, 'Update data is required');
+    }
+
+    const updatedUser = await User.updateUser(loggedIn!, updates);
+
+    res.status(200).json({
+        message: 'Profile updated successfully',
+        updatedUser,
+    });
+});
+
+export const deleteUser = asyncHandler(async (req: Request, res: Response) => {
         const { id } = req.params;
         if (!id) {
-            return res.status(400).json({ message: 'User id is required' });
-        }
-
-        const user = await User.getUserById(id);
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-        }
-        
-        res.status(200).json({
-            message: 'User fetched successfully',
-            user,
-        });
-
-    } catch (err) {
-        if (err instanceof Error) {
-            console.error('Failed to fetch user: ', err.message, err.stack);
-            res.status(500).json({ error: 'server error!', details: err.message || err.toString() });
-        }
-
-    }
-}
-
-export async function getUserProfile(req: Request, res: Response) {
-    try {
-        const userId = req.user?.id;
-         if (!userId) {
-            return res.status(401).json({ error: 'Unauthorized - User ID not found' });
-        }
-
-        const userProfile = await User.getUserProfile(userId);
-        if (!userProfile) {
-            return res.status(404).json({ message: 'No user found' });
-        }
-
-        res.status(200).json({
-            message: 'Public profile fetched successfully',
-            userProfile,
-        });
-
-    } catch (err) {
-        if (err instanceof Error) {
-            console.error('Failed to fetch user profile: ', err.message, err.stack);
-            res.status(500).json({ error: 'server error!', details: err.message || err.toString() });
-        }
-
-    }
-}
-
-export async function updateUserById(req: Request, res: Response) {
-    try {
-        const { id } = req.params;
-        if (!id) {
-            res.status(400).json({ message: 'User id is required' });
-        }
-
-        const updates = req.body;
-        if (!updates) {
-            return res.status(400).json({ message: 'Update data is required' });
-        }
-        
-        const updatedUser = await User.updateUser(String(id), updates);
-        if (!updatedUser) {
-            return res.status(404).json({ message: 'User not found or update failed' });
-        }
-
-        res.status(200).json({
-            message: 'User updated successfully',
-            updatedUser,
-        });
-
-    } catch (err) {
-        if (err instanceof Error) {
-            console.error('Failed to fetch update user: ', err.message, err.stack);
-            res.status(500).json({ error: 'server error!', details: err.message || err.toString() });
-        }
-
-    }
-}
-
-export async function updateUserProfile(req: Request, res: Response) {
-    try {
-        const loggedIn = req.user?.id;
-        if (!loggedIn) {
-            return res.status(400).json({ message: 'Profile id is required' });
-        }
-
-        const updates = req.body;
-        if (!updates) {
-            return res.status(400).json({ message: 'Update data is required' });
-        }
-
-        const updatedUser = await User.updateUser(loggedIn!, updates);
-
-        res.status(200).json({
-            message: 'Profile updated successfully',
-            updatedUser,
-        });
-
-    } catch (err) {
-        if (err instanceof Error) {
-            console.error('Failed to fetch update user: ', err.message, err.stack);
-            res.status(500).json({ error: 'server error!', details: err.message || err.toString() });
-        }
-
-    }
-}
-
-export async function deleteUser(req: Request, res: Response) {
-    try {
-        const { id } = req.params;
-        if (!id) {
-            return res.status(400).json({ message: 'User id is required' });
+            throw new apiError(400, 'User id is required');
         }
 
         const deletedUser = await User.deleteUser(id);
         if (!deletedUser) {
-            return res.status(404).json({ message: 'User not found or could not be deleted' });
+            throw new apiError(404, 'User not found or could not be deleted');
         }
 
         res.status(200).json({
             message: 'User deleted succesfully',
             deletedUser,
         });
-
-    } catch (err) {
-        if (err instanceof Error) {
-            console.error('Failed to fetch delete user: ', err.message, err.stack);
-            res.status(500).json({ error: 'server error!', details: err.message || err.toString() });
-        }
-
-    }
-}
+});
